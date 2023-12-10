12652091f389993887d145228718a060b91220bb
bill <bill.arnold@student.moringaschool.com>
2023-12-10 17:29:23 +0300

    done

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,51 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        # SQL statement to create the 'songs' table
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        # Execute the SQL statement
+        CURSOR.execute(sql)
+
+    def save(self):
+        # SQL statement to insert a new record into the 'songs' table
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        # Execute the SQL statement with bound parameters
+        CURSOR.execute(sql, (self.name, self.album))
+        
+        # Get the ID of the last inserted row and assign it to the instance's id attribute
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
+    @classmethod
+    def create(cls, name, album):
+        # Create a new Song instance and save it to the database
+        song = Song(name, album)
+        song.save()
+        return song
+
+# Example usage:
+# Create the 'songs' table
+Song.create_table()
+
+# Create and save a new song
+hello = Song.create("Hello", "25")
+
+# Retrieve the saved song's attributes
+print(hello.id)    # Should print the assigned ID
+print(hello.name)  # Should print "Hello"
+print(hello.album)  # Should print "25"
